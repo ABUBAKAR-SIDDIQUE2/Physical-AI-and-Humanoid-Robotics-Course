8f47c7966312fd5694bb551f977da7a60044f266
ABUBAKAR-SIDDIQUE2 <a3325452033@gmail.com>
2025-12-07 12:43:57 +0500

    remove unnecesary text

--- physical-ai-book/src/pages/index.tsx
+++ physical-ai-book/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/introduction-to-physical-ai">
-            Start Learning - 5min ⏱️
+            Start Learning
           </Link>
         </div>
       </div>
