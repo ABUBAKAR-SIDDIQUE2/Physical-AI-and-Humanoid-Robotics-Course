17f953a2e3c029e6703ea158cebc36db445b35d8
ABUBAKAR-SIDDIQUE2 <a3325452033@gmail.com>
2025-12-09 00:16:56 +0500

    changed the port for deployment

--- chatbot-backend/run_app.py
+++ chatbot-backend/run_app.py
@@ -7,4 +7,5 @@ if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
+    port = int(os.getenv("PORT"))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
