12f10fec76acd77c2fd49d2054890b478ed5849d
ABUBAKAR-SIDDIQUE2 <a3325452033@gmail.com>
2025-12-10 11:33:03 +0500

    fixed env imports

--- physical-ai-book/src/services/auth-client.ts
+++ physical-ai-book/src/services/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.AUTH_URL, // In production this should be env var
+  baseURL: process.env.AUTH_URL, // In production this should be env var
 });
