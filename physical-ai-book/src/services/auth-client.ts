febf9f9d6d9c31ab32c78906735582243770a60d
ABUBAKAR-SIDDIQUE2 <a3325452033@gmail.com>
2025-12-10 22:33:21 +0500

    updated varialbes

--- physical-ai-book/src/services/auth-client.ts
+++ physical-ai-book/src/services/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 import siteConfig from '@generated/docusaurus.config';
 
-const { VITE_AUTH_URL } = siteConfig.customFields as { VITE_AUTH_URL?: string };
+const { PUBLIC_AUTH_URL } = siteConfig.customFields as { PUBLIC_AUTH_URL?: string };
 
 export const authClient = createAuthClient({
-  baseURL: VITE_AUTH_URL || "http://localhost:4000", // Fallback to local auth default
+  baseURL: PUBLIC_AUTH_URL || "http://localhost:4000", // Fallback to local auth default
 });
